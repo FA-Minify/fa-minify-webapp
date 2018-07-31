15d13b0abf2de20d8474362a90960c657da565c7
Markus Seiffert <m.seiffert@snapaddy.com>
2018-07-31 23:51:24 +0200

    feat: add filter column (not working yet!)

--- src/app/components/choose/choose.component.ts
+++ src/app/components/choose/choose.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { IconService } from '../../services/icon.service';
 import { IconType } from '../../../../node_modules/fa-minify/dist';
+import { CATEGORIES } from '../../data/categories.constants';
+import { ICONS } from '../../data/icons.constants';
 
 @Component({
   selector: 'fm-choose',
@@ -8,6 +10,8 @@ import { IconType } from '../../../../node_modules/fa-minify/dist';
   styleUrls: ['./choose.component.scss']
 })
 export class ChooseComponent {
+  public categories = CATEGORIES;
+  public allIcons = ICONS;
 
   public filter: string;
   public types = ['fal', 'far', 'fas', 'fab'];
@@ -28,6 +32,10 @@ export class ChooseComponent {
 
   public toggleSelection(type: IconType, iconName: string) {
     this.selectedIcons[type][iconName] = !this.selectedIcons[type][iconName];
+  }
+
+  public hasIcons(type: IconType) {
+    return this.icons && this.icons[type] && Object.keys(this.icons[type] || {}).length > 0;
   }
 
 }
